29d7cd6008fc086ed3f3d9713e4ddd06aadb36dc
kang.cao <1457857389@qq.com>
2024-02-07 12:52:16 +0800

    缓存击穿-利用互斥锁解决

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -33,21 +33,90 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      */
     @Override
     public Result queryById(Long id) {
-        // 1.从redis查询店铺缓存
+        // 缓存穿透
+        // Shop shop = queryWithCachePenetrate(id);
+
+        // 缓存击穿-互斥锁解决方案
+        Shop shop = queryWithCacheBreakdownWithMutex(id);
+        if (shop == null) {
+            return Result.fail("店铺不存在");
+        }
+        return Result.ok(shop);
+    }
+
+    /**
+     * 根据id查询店铺信息（缓存击穿--互斥锁解决方案）
+     *
+     * @param id
+     * @return
+     */
+    public Shop queryWithCacheBreakdownWithMutex(Long id) {
         String key = RedisConstants.CACHE_SHOP_KEY + id;
+        // 1.从redis查询店铺缓存
         String shopJson = stringRedisTemplate.opsForValue().get(key);
         // 2.判断缓存中是否有数据
         if (StrUtil.isNotBlank(shopJson)) {
-            // 3.查到的数据非空、非null，返回缓存数据
+            // 3.查到的数据非空字符串、非null，返回缓存数据
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return shop;
         }
-        // 判断缓存命中的是否是空值 (shopJson要么是空字符串""，要么为null)
+        // 判断缓存命中的是否为null (shopJson要么是空字符串""，要么为null)
+        if (shopJson != null) {
+            return null;    // 空字符串，返回错误信息
+        }
+        // 4.实现缓存重建（shopJson为null，缓存无数据）
+        // 4.1 获取互斥锁
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        Shop shop = null;
+        try {
+            boolean getLock = tryLock(lockKey);
+            // 4.2 判断是否获取成功
+            if (!getLock) {
+                // 4.3 获取锁失败，休眠并重试
+                Thread.sleep(50);
+                // 必须在递归调用后立即返回调用的结果！否则线程查到缓存后，还会执行下面的查询数据库操作
+                return queryWithCacheBreakdownWithMutex(id);
+            }
+            // 4.4 获取锁成功，根据id查询数据库
+            shop = getById(id);
+            Thread.sleep(200);  // 模拟查询数据库耗时
+            // 5. 数据库中不存在，将null值写入redis（防止缓存穿透）
+            if (shop == null) {
+                stringRedisTemplate.opsForValue().set(key, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+            }
+            // 6. 数据库中存在，写入redis，设置过期时间
+            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // 7. 释放锁
+            unlock(lockKey);
+        }
+        // 8. 返回
+        return shop;
+    }
+
+    /**
+     * 根据id查询店铺信息（缓存穿透解决方案）
+     *
+     * @param id
+     * @return
+     */
+    public Shop queryWithCachePenetrate(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        // 1.从redis查询店铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2.判断缓存中是否有数据
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 3.查到的数据非空字符串、非null，返回缓存数据
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return shop;
+        }
+        // 判断缓存命中的是否为null (shopJson要么是空字符串""，要么为null)
         if (shopJson != null) {
             // 空值，返回错误信息
-            return Result.fail("店铺不存在");
+            return null;
         }
-
         // 4.缓存无，根据id查询数据库
         Shop shop = getById(id);
         // 5.数据库不存在，将null写入redis（防止缓存穿透）
@@ -55,12 +124,12 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             // 空值写入redis
             stringRedisTemplate.opsForValue().set(key, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
             // 返回错误信息
-            return Result.fail("店铺不存在");
+            return null;
         }
         // 6.存在，写入redis，设置过期时间
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
         // 7.返回
-        return Result.ok(shop);
+        return shop;
     }
 
     private boolean tryLock(String key) {
